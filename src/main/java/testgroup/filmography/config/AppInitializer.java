3c1245309ec4775b8ca6191a804c4199883ef4cd
zhekon19 <zhekon19@gmail.com>
2021-10-27 14:51:58 +0300

    work lesson2

--- src/main/java/testgroup/filmography/config/AppInitializer.java
+++ src/main/java/testgroup/filmography/config/AppInitializer.java
@@ -8,7 +8,7 @@ import javax.servlet.Filter;
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{HibernateConfig.class};
     }
 
     @Override
@@ -27,5 +27,6 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
         characterEncodingFilter.setForceEncoding(true);
         return new Filter[] {characterEncodingFilter};
     }
+
 
 }
