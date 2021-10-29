5e6e4b5f8b10b01355339f5f103367bf32bbb4b7
zhekon19 <zhekon19@gmail.com>
2021-10-29 13:43:41 +0300

    Post,Delete,Put

--- src/main/java/testgroup/filmography/controller/EmployeeController.java
+++ src/main/java/testgroup/filmography/controller/EmployeeController.java
@@ -30,6 +30,25 @@ public class EmployeeController {
         }
         return employee;
     }
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
 
+        Employee employee = employeeService.getEmployee(id);
+        if(employee == null){
+            throw  new NoSuchEmployeeException("There is no employee with id " + id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Delete Employee with id: "+ id + " was deleted";
+    }
 
 }
